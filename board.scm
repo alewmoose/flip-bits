0bf91d57a9d3c44609306fb2badf96ab710d8943
Alex Grehov <alewmoose@gmail.com>
2019-02-17 17:48:48 +0300

    Random board: update nums

--- board.scm
+++ board.scm
@@ -1,59 +1,24 @@
-(module board (make-blank-board make-rand-board board-pp)
+(module board (make-blank-board make-rand-board)
 
 	(import
     (scheme)
     (chicken base)
     (chicken format)
-    (chicken time)
-    (chicken pretty-print)
+    (chicken bitwise)
     (chicken random))
 
   (define-record board size bits row-nums col-nums)
 
-  (define-syntax for
-    (syntax-rules (= to)
-      ((_ var = start to end . forms)
-       (let loop ((var start))
-         (if (<= var end)
-           (begin
-             (begin . forms)
-             (loop (add1 var))))))))
-
   (define (make-bits size)
     (let ((bits (make-vector size)))
-      (for n = 0 to (- size 1)
-        (vector-set! bits n (make-vector size 0)))
+      (let loop ((n 0))
+        (when (< n size)
+          (vector-set! bits n (make-vector size 0))
+          (loop (add1 n))))
       bits))
 
   (define (make-nums size)
     (make-vector size 0))
-
-  (define (board-pp b)
-    (define (nums->str nums)
-      (foldl string-append ""
-             (map (lambda (n) (sprintf "~a " n))
-                  (vector->list nums))))
-    (define (pp-nums name v) (printf "~a: ~a~%" name (nums->str v)))
-    (define (p-header b) (printf "Size: ~a~%" (board-size b)))
-    (define (p-bits b)
-      (let ((size (board-size b)))
-        (for y = 0 to (- size 1)
-          (for x = 0 to (- size 2)
-            (printf "~a " (board-bit b y x)))
-          (printf "~a~%" (board-bit b y (- size 1))))))
-    (define (p-footer b)
-      (pp-nums "rows" (board-row-nums b))
-      (pp-nums "cols" (board-col-nums b)))
-    (define (p-vline) (printf "----~%"))
-    (newline)
-    (p-vline)
-    (p-header b)
-    (p-vline)
-    (p-bits b)
-    (p-vline)
-    (p-footer b)
-    (p-vline)
-    (newline))
 
   (define (board-bit b row col)
     (vector-ref
@@ -61,8 +26,20 @@
       col))
 
   (define (board-bit-set! b row col val)
-    (vector-set! (vector-ref (board-bits b) row) col val))
-
+    (define (nums-set! nums numi i val)
+      (let ((num (vector-ref nums numi))
+            (biti (- (board-size b) i 1)))
+        (vector-set!
+          nums
+          numi
+          (if (zero? val)
+            (bitwise-and num
+                         (bitwise-not (arithmetic-shift 1 biti)))
+            (bitwise-ior num
+                        (arithmetic-shift 1 biti))))))
+    (vector-set! (vector-ref (board-bits b) row) col val)
+    (nums-set! (board-row-nums b) row col val)
+    (nums-set! (board-col-nums b) col row val))
 
   (define (make-blank-board size)
     (make-board
@@ -81,9 +58,9 @@
             (board-bit-set! b y x 1))))
     (define (board-rand-fill! b size)
       (let* ((min-nr (floor (* nr-cells 1/4)))
-            (max-nr (floor (* nr-cells 3/4)))
-            (to-set (+ min-nr
-                       (pseudo-random-integer (- max-nr min-nr)))))
+             (max-nr (floor (* nr-cells 3/4)))
+             (to-set (+ min-nr
+                        (pseudo-random-integer (- max-nr min-nr)))))
         (let loop ((i to-set))
           (if (> i 0)
               (begin
